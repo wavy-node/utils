5d45b15acd73819f176444928eb3aa3db13d4b9f
Manuel Rivera <menu.rivera@gmail.com>
2025-10-06 14:50:17 -0600

    feat: added mexican legislation fields to IUserData

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -19,12 +19,64 @@ export interface IUserData {
 		* ISO 3166 A-2 Country code
 	*/
 	nationality: string,
+	phoneNumber: {
+		countryCode: string,
+		phoneNumber: number,
+	},
+	email: string,
+	address: {
+		/**
+			* ISO 3166 A-2 Country code
+		*/
+		country: string,
+		/**
+			* AKA state or province
+		*/
+		region: string,
+		city: string,
+		street: string,
+		/**
+			* AKA neighborhood
+		*/
+		colonia: string,
+		exteriorNumber: string,
+		interiorNumber?: string,
+		postalCode: string,
+	},
 	/**
-		* Mexican RFC (Registro Federal de Contribuyentes)
+		* Mexican legislation fields, required if mexican legislations is enabled in the dashboard.
+		* A lot of the fields must comply with the specs described in https://sppld.sat.gob.mx/pld/interiores/activos.html
 	*/
-	rfc?: string,
-	/**
-		* Mexican CURP (Clave Única de Registro de Población)
-	*/
-	curp?: string,
+	mexico?: {
+		/**
+			* Mexican RFC (Registro Federal de Contribuyentes)
+		*/
+		rfc?: string,
+		/**
+			* Mexican CURP (Clave Única de Registro de Población)
+		*/
+		curp: string,
+		/**
+			* Key for the economic activity or ocupation of the user.
+			* Check https://sppld.sat.gob.mx/pld/interiores/activos.html for reference.
+		*/
+		actividadEconomica: number,
+		/**
+			* Bank account related with the user
+		*/
+		cuentaRelacionada: string,
+		/**
+			* Clabe interbancaria of the relatedAccount
+		*/
+		clabeInterbancaria?: undefined,
+		/**
+			* Currency of the related bank account, 1 -> MXN, 2 -> USD.
+			* Check https://sppld.sat.gob.mx/pld/interiores/activos.html for reference.
+		*/
+		monedaCuentaRelacionada: 1,
+		documentoIdentificacion: {
+			tipoIdentificacion: number,
+			numeroIdentificacion: string
+		},
+	}
 }
